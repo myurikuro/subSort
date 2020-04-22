2e7fcb5e5aab8c4b87523d7d1f89df80224f0504
zum <killerbee450@gmail.com>
2020-04-22 13:56:15 +0200

    Added 3 tests

--- src/test/java/de/kohnlehome/SubSortTest.java
+++ src/test/java/de/kohnlehome/SubSortTest.java
@@ -16,13 +16,28 @@ public class SubSortTest {
 
     @Test
     public void test() {
-        int[] diff1 = {1, 3, 15, 11, 2};
+        int[] diff1 = {1, 2, 4, 7, 10, 11, 7, 12, 6, 7, 16, 18, 19};
+
+        int[] diff2 = {11, 7, 12, 6, 7, 16, 18, 19};
+
+        int[] diff3 = {1, 2, 4, 5, 7, 8, 12, 13, 10, 14, 15};
 
 
         //Act
         int[] difference1 = subSort.subSort(diff1);
 
+        int[] difference2 = subSort.subSort(diff2);
+
+        int[] difference3 = subSort.subSort(diff3);
+
         //Assert
-        assertThat(difference1).isEqualTo(6);
+        int[] answer1 = {3, 9};
+        assertThat(difference1).isEqualTo(answer1);
+
+        int[] answer2 = {0, 4};
+        assertThat(difference2).isEqualTo(answer2);
+
+        int[] answer3 = {5, 7};
+        assertThat(difference3).isEqualTo(answer3);
     }
 }
